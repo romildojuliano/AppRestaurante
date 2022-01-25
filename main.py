305a5300599d2a9fa39ef50605704d8b0aec0e35
rjal <rjal@cin.ufpe.br>
2022-01-25 20:44:13 -0300

    Qrcode sendo gerado; servidor funcionando

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import tkinter as tk
+import os
+import io
+import qrcode
+import info
 
-menu_inicial = tk.Tk()
-menu_inicial.mainloop()
+print("IP: {}".format(info.my_ip))
+print("PORT: {}".format(info.PORT))
+
+link = 'http://{}:{}'.format(info.my_ip, info.PORT)
+
+qr = qrcode.QRCode()
+qr.add_data(link)
+f = io.StringIO()
+qr.print_ascii(out=f)
+f.seek(0)
+print("Link gerado: ")
+print(f.read())
+
+os.system("python3.10 -m http.server {}".format(info.PORT))
---
+++ table.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import io
+import os
+import info
+import qrcode
+from string import ascii_letters, digits
+from typing import List
+from random import choices
+
+class Table:
+    id: int
+    tableId: str
+    URL: str
+    requests: List[int]
+    infoClients: pd.DataFrame
+    QRCode: io.StringIO
+
+    def __init__(self) -> None:
+        self.infoClients = pd.read_csv("tables.csv", sep=';')
+        self.id = self.infoClients.__len__()
+        
+        self.tableId = ''.join(choices(ascii_letters + digits, k=8))
+        os.system("mkdir tables/{}".format(self.tableId))
+        
+        self.requests = list()
+        self.URL = 'http://{}:{}/tables/{}/'.format(info.my_ip, info.PORT, self.tableId)
+        
+        qr = qrcode.QRCode()
+        qr.add_data(self.URL)
+        
+        self.QRCode = io.StringIO()
+        qr.print_ascii(out=self.QRCode)
+            
+        self.update_tables()
+        pass   
+    def __str__(self, QR: bool) -> str:
+        self.infoClients = pd.read_csv("tables.csv", sep=';')
+        str =  self.infoClients.iloc[self.id].to_string()
+        if (QR):
+            self.QRCode.seek(0)
+            return str + self.QRCode.read()
+        else: return str        
+    def update_tables(self) -> None:
+        with open("tables.csv", "a") as f:
+            f.write('{};{};{}\n'.format(self.id, self.tableId, self.URL))
+
+if __name__ == "__main__":
+    x = Table()
+    print(x.__str__(QR=True))
+    pass
